8fe42aecb90fbeb2133cbb8a3e1d24c6724e9261
LoaiAlaa24 <loai.alaa7@gmail.com>
2018-11-28 01:09:37 +0200

    added some changes and pages

--- main.go
+++ main.go
@@ -38,8 +38,11 @@ type Gopher struct{
 }
 type MealDetails struct {
 	Name  string
-	Subject string
-	Message string
+}
+type ExDetails struct {
+	Name  string
+	Age  string
+
 }
 type Responser struct {
 	Response string
@@ -54,50 +57,6 @@ func envOrDefault(key, defaultValue string) string {
 	return defaultValue
 }
 
-func myHandler(w http.ResponseWriter, r *http.Request) {
-
-	tmpl := template.Must(template.ParseFiles("forms.html"))
-
-	if r.Method != http.MethodPost {
-		tmpl.Execute(w, nil)
-		return
-	}
-
-	details := MealDetails{
-		Name:   r.FormValue("name"),
-		Subject: r.FormValue("subject"),
-		Message: r.FormValue("message"),
-	}
-
-	// do something with details
-	_ = details
-	log.Println(details.Name)
-		jsonData := map[string]string{"query": details.Name}
-	jsonValue, _ := json.Marshal(jsonData)
-	request, _ := http.NewRequest("POST", "https://trackapi.nutritionix.com/v2/natural/nutrients", bytes.NewBuffer(jsonValue))
-	request.Header.Set("Content-Type", "application/json")
-	request.Header.Set("x-app-id", "40523543")
-	request.Header.Set("x-app-key", "44d9799d0bf08ca4a633dff233675a3d")
-	request.Header.Set("x-remote-user-id", "0")
-	client := &http.Client{}
-	response, err := client.Do(request)
-	if err != nil {
-		log.Fatal("The HTTP request failed with error %s\n", err)
-		z:= Responser{err.Error(),false}
-		tmpl.Execute(w, z)
-	} else {
-		data, _ := ioutil.ReadAll(response.Body)
-		log.Println(string(data))
-		z:= Responser{string(data),true}
-		tmpl.Execute(w, z)
-	}
-
-	
-	// t, _:= template.ParseFiles("forms.html")
-	// t.Execute(w, "Hello World!")
-
-}
-
 func myHandler2(w http.ResponseWriter, r *http.Request) {
 
 	tmpl := template.Must(template.ParseFiles("forms2.html"))
@@ -109,8 +68,51 @@ func myHandler2(w http.ResponseWriter, r *http.Request) {
 
 	details := MealDetails{
 		Name:   r.FormValue("name"),
-		Subject: r.FormValue("subject"),
-		Message: r.FormValue("message"),
+	}
+	exdetails := ExDetails{
+		Name:   r.FormValue("name"),
+		Age: r.FormValue("age"),
+	}
+	// do something with details
+	_ = details
+	log.Println(details.Name)
+	jsonData := map[string]string{"query": exdetails.Name , "age":exdetails.Age}
+	jsonValue, _ := json.Marshal(jsonData)
+	request, _ := http.NewRequest("POST", "https://trackapi.nutritionix.com/v2/natural/exercise", bytes.NewBuffer(jsonValue))
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("x-app-id", "40523543")
+	request.Header.Set("x-app-key", "44d9799d0bf08ca4a633dff233675a3d")
+	request.Header.Set("x-remote-user-id", "0")
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		log.Fatal("The HTTP request failed with error %s\n", err)
+		z:= Responser{err.Error(),false}
+		tmpl.Execute(w, z)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		log.Println(string(data))
+		z:= Responser{string(data),true}
+		tmpl.Execute(w, z)
+	}
+
+	
+	// t, _:= template.ParseFiles("forms.html")
+	// t.Execute(w, "Hello World!")
+
+}
+
+func myHandler(w http.ResponseWriter, r *http.Request) {
+
+	tmpl := template.Must(template.ParseFiles("forms.html"))
+
+	if r.Method != http.MethodPost {
+		tmpl.Execute(w, nil)
+		return
+	}
+
+	details := MealDetails{
+		Name:   r.FormValue("name"),
 	}
 
 	// do something with details
@@ -143,11 +145,51 @@ func myHandler2(w http.ResponseWriter, r *http.Request) {
 }
 
 
+func myHandlerMenu(w http.ResponseWriter, r *http.Request) {
+
+	tmpl := template.Must(template.ParseFiles("menu.html"))
+
+	if r.Method != http.MethodPost {
+		tmpl.Execute(w, nil)
+		return
+	}
+
+	// details := MealDetails{
+	// 	Name:   r.FormValue("name"),
+	// }
+
+	// // do something with details
+	// _ = details
+	// log.Println(details.Name)
+	// 	jsonData := map[string]string{"query": details.Name}
+	// jsonValue, _ := json.Marshal(jsonData)
+	// request, _ := http.NewRequest("POST", "https://trackapi.nutritionix.com/v2/natural/nutrients", bytes.NewBuffer(jsonValue))
+	// request.Header.Set("Content-Type", "application/json")
+	// request.Header.Set("x-app-id", "40523543")
+	// request.Header.Set("x-app-key", "44d9799d0bf08ca4a633dff233675a3d")
+	// request.Header.Set("x-remote-user-id", "0")
+	// client := &http.Client{}
+	// response, err := client.Do(request)
+	// if err != nil {
+	// 	log.Fatal("The HTTP request failed with error %s\n", err)
+	// 	z:= Responser{err.Error(),false}
+	// 	tmpl.Execute(w, z)
+	// } else {
+	// 	data, _ := ioutil.ReadAll(response.Body)
+	// 	log.Println(string(data))
+	// 	z:= Responser{string(data),true}
+	// 	tmpl.Execute(w, z)
+	// }
+
+	
+	// t, _:= template.ParseFiles("forms.html")
+	// t.Execute(w, "Hello World!")
+
+}
 
 func main() {
-
-
-	http.HandleFunc("/", myHandler)
+	http.HandleFunc("/", myHandlerMenu)
+	http.HandleFunc("/meal", myHandler)
 	http.HandleFunc("/exercise", myHandler2)
 	log.Print("Listening on " + webHost + ":" + webPort + "...")
 	http.ListenAndServe(webHost+":"+webPort, nil)
